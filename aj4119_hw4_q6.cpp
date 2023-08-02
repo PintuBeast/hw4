ea841d1c0c0c26960e541895596964a8816f31a7
pintubeast <abhishek.jk1992@gmail.com>
2023-08-02 22:43:57 +0530

    q6

---
+++ aj4119_hw4_q6.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,numEven,numOdd;
+    cout<<"Please enter a positive integer n:";
+    cin>>n;
+    for (int i=1;i<=n;i++)
+    {  int temp = i;
+       numEven = 0;
+       numOdd = 0; 
+        while (temp>0)
+        {
+            if(temp % 2 == 0)
+            {
+                numEven++;
+            }
+            else{
+                numOdd++;
+            }
+            temp=temp / 10;
+
+        }
+        if(numEven>numOdd)
+        {
+            cout<<i<<"\n";
+        }
+    }
+
+}
